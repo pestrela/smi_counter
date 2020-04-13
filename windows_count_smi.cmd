c3fa23685cfe10aa327dae2054676a454c9e3d8a
Pedro Estrela <pedro.estrela@gmail.com>
2020-04-13 14:51:01 +0100

    fast upload

--- windows_count_smi.cmd
+++ windows_count_smi.cmd
@@ -1,12 +1,15 @@
 @echo off
 
-echo ""
-echo "SMI counter - batch file version"
-echo ""
+@echo.
+@echo "SMI counter - batch file version"
+@echo.
 
 :loop
+
+@echo.
+@echo Date: %date% %time%
 "c:\Program Files (x86)\Windows Kits\10\Debuggers\x64\kd.exe" -kl -c "RDMSR 0x34; q" | find "msr"
 
-set /P name="Do something, and then press 'enter' to read the MSI again"
+set /P name="press CTRL+C to exit, Enter to measure again"
 goto loop
 
